3a0b20051ba2067742003481ac131f090b46a53d
ShaziaGhaffour <shaziaghaffour1@gmail.com>
2025-08-13 16:41:26 -0700

    message

--- src/pages/offers.jsx
+++ src/pages/offers.jsx
@@ -1,112 +1,26 @@
 import React from 'react';
-// import Footer from './footer';
+import CameraImage from '../assets/camera.svg';
+import FrameImage from '../assets/Frame 11.svg';
 
 const Offers = () => {
   return (
-    <>
-            <div className="flex flex-wrap justify-center gap-6 mb-5 mt-5">
-              <div className="flex flex-col bg-white items-center w-[232px] p-2 shadow-lg rounded-lg">
-                <img src="/public/2022-01-01_23-08-54_-2-removebg-preview.svg" alt="Camera" className="w-full mb-2" />
-                <img src="/Frame 11.svg" alt="Frame" className="mb-2 self-start" />
-                <h3 className="text-left text-black" style={{ fontSize: "12px", fontWeight: "400" }}>
-                  System 16 Camera 2MP HD + Installation and
-                  configuration Fee
-                </h3>
-              </div>
-              <div className="flex flex-col bg-white items-center w-[232px] p-2 shadow-lg rounded-lg">
-                <img src="/public/2022-01-01_23-08-54_-2-removebg-preview.svg" alt="Camera" className="w-full mb-2" />
-                <img src="/Frame 11.svg" alt="Frame" className="mb-2 self-start" />
-                <h3 className="text-left text-black" style={{ fontSize: "12px", fontWeight: "400" }}>
-                  System 16 Camera 2MP HD + Installation and
-                  configuration Fee
-                </h3>
-              </div>
-              <div className="flex flex-col bg-white items-center w-[232px] p-2 shadow-lg rounded-lg">
-                <img src="/public/2022-01-01_23-08-54_-2-removebg-preview.svg" alt="Camera" className="w-full mb-2" />
-                <img src="/Frame 11.svg" alt="Frame" className="mb-2 self-start" />
-                <h3 className="text-left text-black" style={{ fontSize: "12px", fontWeight: "400" }}>
-                  System 16 Camera 2MP HD + Installation and
-                  configuration Fee
-                </h3>
-              </div>
-              <div className="flex flex-col items-center w-[232px] pt-4 pb-4 px-2 shadow-lg rounded-lg">
-                <img src="/public/2022-01-01_23-08-54_-2-removebg-preview.svg" alt="Camera" className="w-full mb-2" />
-                <img src="/Frame 11.svg" alt="Frame" className="mb-2 self-start" />
-                <h3 className="text-left text-black" style={{ fontSize: "12px", fontWeight: "400" }}>
-                  System 16 Camera 2MP HD + Installation and
-                  configuration Fee
-                </h3>
-              </div>
-            </div>
-              <div className="flex flex-wrap justify-center gap-6 mb-5 mt-5">
-              <div className="flex flex-col bg-white items-center w-[232px] p-2 shadow-lg rounded-lg">
-                <img src="/public/2022-01-01_23-08-54_-2-removebg-preview.svg" alt="Camera" className="w-full mb-2" />
-                <img src="/Frame 11.svg" alt="Frame" className="mb-2 self-start" />
-                <h3 className="text-left text-black" style={{ fontSize: "12px", fontWeight: "400" }}>
-                  System 16 Camera 2MP HD + Installation and
-                  configuration Fee
-                </h3>
-              </div>
-              <div className="flex flex-col bg-white items-center w-[232px] p-2 shadow-lg rounded-lg">
-                <img src="/public/2022-01-01_23-08-54_-2-removebg-preview.svg" alt="Camera" className="w-full mb-2" />
-                <img src="/Frame 11.svg" alt="Frame" className="mb-2 self-start" />
-                <h3 className="text-left text-black" style={{ fontSize: "12px", fontWeight: "400" }}>
-                  System 16 Camera 2MP HD + Installation and
-                  configuration Fee
-                </h3>
-              </div>
-              <div className="flex flex-col bg-white items-center w-[232px] p-2 shadow-lg rounded-lg">
-                <img src="/public/2022-01-01_23-08-54_-2-removebg-preview.svg" alt="Camera" className="w-full mb-2" />
-                <img src="/Frame 11.svg" alt="Frame" className="mb-2 self-start" />
-                <h3 className="text-left text-black" style={{ fontSize: "12px", fontWeight: "400" }}>
-                  System 16 Camera 2MP HD + Installation and
-                  configuration Fee
-                </h3>
-              </div>
-              <div className="flex flex-col items-center w-[232px] pt-4 pb-4 px-2 shadow-lg rounded-lg">
-                <img src="/public/2022-01-01_23-08-54_-2-removebg-preview.svg" alt="Camera" className="w-full mb-2" />
-                <img src="/Frame 11.svg" alt="Frame" className="mb-2 self-start" />
-                <h3 className="text-left text-black" style={{ fontSize: "12px", fontWeight: "400" }}>
-                  System 16 Camera 2MP HD + Installation and
-                  configuration Fee
-                </h3>
-              </div>
-            </div>
-              <div className="flex flex-wrap justify-center gap-6 mb-5 mt-5">
-              <div className="flex flex-col bg-white items-center w-[232px] p-2 shadow-lg rounded-lg">
-                <img src="/public/2022-01-01_23-08-54_-2-removebg-preview.svg" alt="Camera" className="w-full mb-2" />
-                <img src="/Frame 11.svg" alt="Frame" className="mb-2 self-start" />
-                <h3 className="text-left text-black" style={{ fontSize: "12px", fontWeight: "400" }}>
-                  System 16 Camera 2MP HD + Installation and
-                  configuration Fee
-                </h3>
-              </div>
-              <div className="flex flex-col bg-white items-center w-[232px] p-2 shadow-lg rounded-lg">
-                <img src="/public/2022-01-01_23-08-54_-2-removebg-preview.svg" alt="Camera" className="w-full mb-2" />
-                <img src="/Frame 11.svg" alt="Frame" className="mb-2 self-start" />
-                <h3 className="text-left text-black" style={{ fontSize: "12px", fontWeight: "400" }}>
-                  System 16 Camera 2MP HD + Installation and
-                  configuration Fee
-                </h3>
-              </div>
-              <div className="flex flex-col bg-white items-center w-[232px] p-2 shadow-lg rounded-lg">
-                <img src="/public/2022-01-01_23-08-54_-2-removebg-preview.svg" alt="Camera" className="w-full mb-2" />
-                <img src="/Frame 11.svg" alt="Frame" className="mb-2 self-start" />
-                <h3 className="text-left text-black" style={{ fontSize: "12px", fontWeight: "400" }}>
-                  System 16 Camera 2MP HD + Installation and
-                  configuration Fee
-                </h3>
-              </div>
-              <div className="flex flex-col items-center w-[232px] pt-4 pb-4 px-2 shadow-lg rounded-lg">
-                <img src="/public/2022-01-01_23-08-54_-2-removebg-preview.svg" alt="Camera" className="w-full mb-2" />
-                <img src="/Frame 11.svg" alt="Frame" className="mb-2 self-start" />
-                <h3 className="text-left text-black" style={{ fontSize: "12px", fontWeight: "400" }}>
-                  System 16 Camera 2MP HD + Installation and
-                  configuration Fee
-                </h3>
-              </div>
-            </div>
-     </>
+    <main className="flex flex-wrap justify-center gap-6 my-5">
+      {Array.from({ length: 12 }).map((_, index) => (
+        <div
+          key={index}
+          className="flex flex-col bg-white items-center w-[232px] p-2 shadow-lg rounded-lg"
+        >
+          <img src={CameraImage} alt="Camera" className="w-full mb-2" />
+          <img src={FrameImage} alt="Frame" className="mb-2 self-start" />
+          <h3
+            className="text-left text-black"
+            style={{ fontSize: '12px', fontWeight: '400' }}
+          >
+            System 16 Camera 2MP HD + Installation and configuration Fee
+          </h3>
+        </div>
+      ))}
+    </main>
   );
 };
 
