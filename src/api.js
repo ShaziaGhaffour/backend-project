dc8c1ca19f7f14423b8fac8f7ca4feea1ed849b9
ShaziaGhaffour <shaziaghaffour1@gmail.com>
2025-08-15 12:21:21 -0700

    message

--- src/api.js
+++ src/api.js
@@ -1,27 +1,8 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "http://localhost:2000", // yahan tumhara backend ka base URL
-//   withCredentials: true, // agar cookies/auth chahiye
-// });
-
-// export default API;
-
-// src/api.js
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:2000",
-  withCredentials: true
-});
-
-// ✅ Token har request me auto add
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+  baseURL: "http://localhost:2000", // yahan tumhara backend ka base URL
+  withCredentials: true, // agar cookies/auth chahiye
 });
 
 export default API;
