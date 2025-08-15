fb2db91bd7084b9bca1322d0ffa171ac15454c43
ShaziaGhaffour <shaziaghaffour1@gmail.com>
2025-08-15 11:02:16 -0700

    message

--- src/api.js
+++ src/api.js
@@ -1,11 +1,24 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:2000", // yahan tumhara backend ka base URL
+//   withCredentials: true, // agar cookies/auth chahiye
+// });
+
+// export default API;
+
+// api.js
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:2000", // yahan tumhara backend ka base URL
-  withCredentials: true, // agar cookies/auth chahiye
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:2000"
+    : "https://backend-project-dotl.vercel.app",
+  withCredentials: true,
 });
 
 export default API;
+
 
 
 
